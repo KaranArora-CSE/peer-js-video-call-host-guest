15ff2d7e98a44062add24712a4f94143699d1924
karan-laptop-dell <cse.karanarora@gmail.com>
2024-04-20 13:03:50 +0530

    added correct links

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,16 +1,13 @@
 import React from 'react'
+import { Link } from "react-router-dom";
 
 function Home() {
     return (
-        <>
+        <div>
             <p>Home</p>
-            <div>
-                <Link to={"/host"}>Host</Link>
-            </div>
-            <div>
-                <Link to={"/guest"}>Guest</Link>
-            </div>
-        </>
+            <div><Link to={"/host"}>Host</Link></div>
+            <div><Link to={"/guest"}>Guest</Link></div>
+        </div>
     )
 }
 
